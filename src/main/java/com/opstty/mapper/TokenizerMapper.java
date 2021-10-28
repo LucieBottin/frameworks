790821f5730d0ec470c643f5db97710f4fc4f051
luciebottin <lucie.bottin@efrei.net>
2021-10-28 15:38:19 +0200

    test

--- src/main/java/com/opstty/mapper/TokenizerMapper.java
+++ src/main/java/com/opstty/mapper/TokenizerMapper.java
@@ -1,17 +1,15 @@
 package com.opstty.mapper;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
 
     public void map(Object key, Text value, Context context)
             throws IOException, InterruptedException {
-        String rounding = value.toString().split(";")[1];
+        String arrondissement = value.toString().split(";")[1];
         context.write(new Text(arrondissement), new Text(" "));
     }
 }
--- src/main/java/com/opstty/reducer/IntSumReducer.java
+++ src/main/java/com/opstty/reducer/IntSumReducer.java
@@ -1,6 +1,5 @@
 package com.opstty.reducer;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
